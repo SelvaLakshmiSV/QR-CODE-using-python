c7f585672a5417eb3b01a173cc6f72dd35649bc3
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-06-11 11:46:07 +0530

    Github QR code

---
+++ tr.py
@@ -0,0 +1,6 @@
+from MyQR import myqr as mq
+mq.run(words = 'https://github.com/SelvaLakshmiSV',
+  version = 6,
+  picture = 'C:/Users/Elcot/Desktop/Good/goog.jpg',
+  colorized = True,
+  save_name = 'github.png')
